e0ed76721125cdfc5ec692b3ddd707da957d81e3
TEC <git@tecosaur.net>
2024-08-05 22:34:56 +0800

    Reduced method overrriding

--- src/strings/basic.jl
+++ src/strings/basic.jl
@@ -5,3 +5,23 @@ _isannotated(s) = _isannotated(typeof(s))
 
 # actually from substring.jl
 _isannotated(::SubString{T}) where {T} = _isannotated(T)
+
+function (*)(s1::Union{AnnotatedChar, AnnotatedString}, s2::Union{AbstractChar, AbstractString})
+    annotatedstring(s1, s2)
+end
+
+function (*)(s1::Union{AbstractChar, AbstractString}, s2::Union{AnnotatedChar, AnnotatedString})
+    annotatedstring(s1, s2)
+end
+
+function (*)(s1::Union{AnnotatedChar, AnnotatedString}, s2::Union{AnnotatedChar, AnnotatedString})
+    annotatedstring(s1, s2)
+end
+
+function (*)(s1::Union{AbstractChar, AbstractString}, s2::Union{AbstractChar, AbstractString}, ss::Union{AbstractChar, AbstractString}...)
+    if _isannotated(s1) || _isannotated(s2) || any(_isannotated, ss)
+        annotatedstring(s1, s2, ss...)
+    else
+        string(s1, s2, ss...)
+    end
+end
