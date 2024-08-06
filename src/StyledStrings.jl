35a3cdf9a585951227932f961ad09fed5f858074
TEC <git@tecosaur.net>
2024-08-07 01:42:22 +0800

    Load user-customisations lazily
    This is primarily motivated by reducing the amount of work that trimming
    is unable to remove.

--- src/StyledStrings.jl
+++ src/StyledStrings.jl
@@ -16,12 +16,29 @@ include("legacy.jl")
 
 using .StyledMarkup
 
-function __init__()
+const HAVE_LOADED_CUSTOMISATIONS = Base.Threads.Atomic{Bool}(false)
+
+"""
+    load_customisations!(; force::Bool=false)
+
+Load customisations from the user's `faces.toml` file, if it exists as well as
+the current environment.
+
+This function should be called before producing any output in situations where
+the user's customisations should be considered.
+
+Unless `force` is set, customisations are only applied when this function is
+called for the first time, and subsequent calls are a no-op.
+"""
+function load_customisations!(; force::Bool=false)
+    !force && HAVE_LOADED_CUSTOMISATIONS[] && return
     if !isempty(DEPOT_PATH)
         userfaces = joinpath(first(DEPOT_PATH), "config", "faces.toml")
         isfile(userfaces) && loaduserfaces!(userfaces)
     end
     Legacy.load_env_colors!()
+    HAVE_LOADED_CUSTOMISATIONS[] = true
+    nothing
 end
 
 if Base.generating_output()
