d63fbd75f1dd7b8b98b0612478435a1b442912de
TEC <git@tecosaur.net>
2024-08-24 14:25:27 +0800

    Improve type stability within styled"" macro
    This way, when interpolating, the type of the expression can be inferred
    as an AnnotatedString, rather than a Union{String, AnnotatedString}.

--- src/styledmarkup.jl
+++ src/styledmarkup.jl
@@ -267,7 +267,7 @@ function addpart!(state::State, start::Int, expr, stop::Int)
                           AnnotatedString(String($str), vcat($annots, annotations($str)))
                       else
                           if isempty($str)
-                              ""
+                              AnnotatedString("")
                           else
                               AnnotatedString($str, $annots)
                           end
