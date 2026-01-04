3d224c764dadf7538974cc9fc135167cff4895d7
TEC <git@tecosaur.net>
2026-01-04 18:03:18 +0800

    Allow fully qualified references to basic faces

--- src/styledmarkup.jl
+++ src/styledmarkup.jl
@@ -46,7 +46,7 @@ Of course, as usual, the devil is in the details.
 module StyledMarkup
 
 using Base: AnnotatedString, annotations, annotatedstring
-using ..StyledStrings: FACES, Face, SimpleColor,
+using ..StyledStrings: FACES, STANDARD_FACES, Face, SimpleColor,
     findface, lookupface, lookmakeface, MAGIC_DEFPALETTE_VARNAME, MAGIC_USEPALETTE_VARNAME
 
 export @styled_str, styled
@@ -511,8 +511,6 @@ function read_inlineface!(state::State, i::Int, _char::Char)
             tryparse(SimpleColor, color)
         elseif startswith(color, "0x") && length(color) == 8
             tryparse(SimpleColor, '#' * color[3:end])
-        elseif !ismacro(state)
-            SimpleColor(get(FACES.pool, Symbol(color), FACES.default))
         else
             resolveface(state, color)
         end
